610c52965036319a5176a5397a47e2c17439f35a
BrandonTang <blt264@nyu.edu>
2017-01-17 17:01:48 -0500

    Added create_db function in manage.py

--- manage.py
+++ manage.py
@@ -84,6 +84,12 @@ def reset_db():
     db.create_all()
 
 
+@manager.command
+def create_db():
+    """Empties the database and generates it again with db_setup"""
+    db.create_all()
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
