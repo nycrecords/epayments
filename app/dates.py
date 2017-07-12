6762225b3faa1f0f9a86eeb0a2bafacbcfa7d121
Arnis Dizdari <arnis.dizdari@gmail.com>
2017-07-12 15:24:55 -0400

    Took out the dates.py file and requirements file fixs

--- app/dates.py
+++
@@ -1,9 +0,0 @@
-"""
-.. module:: constants.status.
-
-    :synopsis: Defines constants used throughout the application
-    :This file will hold the day // month // years to store correct info into database
-"""
-
-
-
