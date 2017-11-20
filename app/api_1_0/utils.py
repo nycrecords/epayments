1fecb02bbe8bc8414e638dec59af15362f23f7ad
Joel Castillo <jocastillo@records.nyc.gov>
2017-11-20 08:58:25 -0500

    WIP: Refactored Order Retrieval; Fixed missing name in models

--- app/api_1_0/utils.py
+++ app/api_1_0/utils.py
@@ -16,48 +16,15 @@ from app.models import (
     PhotoTax,
     PropertyCard
 )
+from app.constants import (
+    order_types
+)
 
 
-def update_status(suborder_no, comment, new_status):
-    """
-        POST: {suborder_no, new_status, comment};
-        returns: {status_id, suborder_no, status, comment}, 201
-
-    Take in the info, this function only gets called if the form is filled
-     - access the db to get the status_id for this particular order
-     - now create a new row in the db in the status table with
-     - this row should have a status_id + 1 then the highest status row
-     - 1) it will have the same suborder_no
-     - 2) it will have the comment that was passed in or None
-     - 3) it will have the new status that was passed from the user
-    """
-
-    object_ = StatusTracker.query.filter_by(suborder_no=suborder_no).order_by(StatusTracker.timestamp.desc()).first()
-
-    if object_ is not None:
-        previous_value = object_.current_status
-    else:
-        previous_value = object_.current_status
-
-    insert_status = StatusTracker(suborder_no=suborder_no,
-                                  current_status=new_status,
-                                  comment=comment,
-                                  timestamp=datetime.utcnow(),
-                                  previous_value=previous_value)
-
-    db.session.add(insert_status)
-    db.session.commit()
-
-
-def get_orders_by_fields(order_no, suborder_no, order_type, billing_name, user, date_submitted_start,
+def _order_query_filters(order_no, suborder_no, order_type, billing_name, user, date_submitted_start,
                          date_submitted_end):
-    """
-    Filter orders by fields received
-    get_orders_by_fields(client_id, suborder_no, order_type(Death Search or Marriage Search), billing_name
-                         user??, date_received, date_submitted)
-    :return:
-    """
-    vital_records_list = ['Birth cert', 'Marriage cert', 'Death cert']
+    # TODO: Need to refactor get_order_by_fields so that it performs a single function for code reuse.
+    vital_records_list = ['Birth cert', 'Marriage cert', 'Death cert'] # TODO: Searches are missing from here. @gzhou??
     photo_list = ['photo tax', 'photo gallery', 'property card']
 
     filter_args = []
@@ -104,15 +71,57 @@ def get_orders_by_fields(order_no, suborder_no, order_type, billing_name, user, 
                 filter_args.append(
                     col.__eq__(value)
                 )
+
+    return filter_args
+
+
+def update_status(suborder_no, comment, new_status):
+    """
+        POST: {suborder_no, new_status, comment};
+        returns: {status_id, suborder_no, status, comment}, 201
+
+    Take in the info, this function only gets called if the form is filled
+     - access the db to get the status_id for this particular order
+     - now create a new row in the db in the status table with
+     - this row should have a status_id + 1 then the highest status row
+     - 1) it will have the same suborder_no
+     - 2) it will have the comment that was passed in or None
+     - 3) it will have the new status that was passed from the user
+    """
+
+    object_ = StatusTracker.query.filter_by(suborder_no=suborder_no).order_by(StatusTracker.timestamp.desc()).first()
+
+    if object_ is not None:
+        previous_value = object_.current_status
+    else:
+        previous_value = object_.current_status
+
+    insert_status = StatusTracker(suborder_no=suborder_no,
+                                  current_status=new_status,
+                                  comment=comment,
+                                  timestamp=datetime.utcnow(),
+                                  previous_value=previous_value)
+
+    db.session.add(insert_status)
+    db.session.commit()
+
+
+def get_orders_by_fields(order_no, suborder_no, order_type, billing_name, user, date_submitted_start,
+                         date_submitted_end):
+    """
+    Filter orders by fields received
+    get_orders_by_fields(client_id, suborder_no, order_type(Death Search or Marriage Search), billing_name
+                         user??, date_received, date_submitted)
+    :return:
+    """
+    filter_args = _order_query_filters(order_no, suborder_no, order_type, billing_name, user, date_submitted_start,
+                         date_submitted_end)
     base_query = Suborder.query.join(Order, Customer).filter(*filter_args)
     order_count = base_query.distinct(Suborder.order_no).group_by(Suborder.order_no, Suborder.id).count()
     suborder_list = base_query.all()
     suborder_count = len(suborder_list)
     return order_count, suborder_count, [suborder.serialize for suborder in suborder_list]
 
-def _order_query():
-    # TODO: Need to refactor get_order_by_fields so that it performs a single function for code reuse.
-    pass
 
 def _print_orders(search_params):
     """
@@ -129,38 +138,40 @@ def _print_orders(search_params):
     billing_name = search_params.get("billing_name")
     # user = str(request.form["user"])
     user = ''
-    date_received = search_params.get("date_received")
-    date_submitted = search_params.get("date_submitted")
+    date_submitted_start = search_params.get("date_submitted_start")
+    date_submitted_end = search_params.get("date_submitted_end")
 
-    order_count, suborder_count, items = get_orders_by_fields(order_number, suborder_number, order_type, billing_name, user, date_received,
-                                  date_submitted)
+    filter_args = _order_query_filters(order_number, suborder_number, order_type, billing_name, user, date_submitted_start,
+                         date_submitted_end)
+
+    suborders = Suborder.join(Customer, Order).filter(*filter_args).all()
 
     order_type_template_handler = {
-        order_type.BIRTH_SEARCH: 'birth_search.html',
-        order_type.BIRTH_CERT: 'birth_certificate.html',
-        order_type.MARRIAGE_SEARCH: 'marriage_search.html',
-        order_type.MARRIAGE_CERT: 'marriage_certificate.html',
-        order_type.DEATH_SEARCH: 'death_search.html',
-        order_type.DEATH_CERT: 'death_certificate.html',
-        order_type.PHOTO_TAX: 'photo_tax.html',
-        order_type.PHOTO_GALLERY: 'photo_gallery.html',
-        order_type.PROPERTY_CARD: 'property_card.html',
+        order_types.BIRTH_SEARCH: 'birth_search.html',
+        order_types.BIRTH_CERT: 'birth_certificate.html',
+        order_types.MARRIAGE_SEARCH: 'marriage_search.html',
+        order_types.MARRIAGE_CERT: 'marriage_certificate.html',
+        order_types.DEATH_SEARCH: 'death_search.html',
+        order_types.DEATH_CERT: 'death_certificate.html',
+        order_types.PHOTO_TAX: 'photo_tax.html',
+        order_types.PHOTO_GALLERY: 'photo_gallery.html',
+        order_types.PROPERTY_CARD: 'property_card.html',
     }
 
     order_type_models_handler = {
-        order_type.BIRTH_SEARCH: BirthSearch,
-        order_type.BIRTH_CERT: BirthCertificate,
-        order_type.MARRIAGE_SEARCH: MarriageSearch,
-        order_type.MARRIAGE_CERT: MarriageCertificate,
-        order_type.DEATH_SEARCH: DeathSearch,
-        order_type.DEATH_CERT: DeathCertificate,
-        order_type.PHOTO_TAX: PhotoTax,
-        order_type.PHOTO_GALLERY: PhotoGallery,
-        order_type.PROPERTY_CARD: PropertyCard,
+        order_types.BIRTH_SEARCH: BirthSearch,
+        order_types.BIRTH_CERT: BirthCertificate,
+        order_types.MARRIAGE_SEARCH: MarriageSearch,
+        order_types.MARRIAGE_CERT: MarriageCertificate,
+        order_types.DEATH_SEARCH: DeathSearch,
+        order_types.DEATH_CERT: DeathCertificate,
+        order_types.PHOTO_TAX: PhotoTax,
+        order_types.PHOTO_GALLERY: PhotoGallery,
+        order_types.PROPERTY_CARD: PropertyCard,
     }
 
-    for item in items:
-        order_info = order_type_models_handler[item.]
+    for item in suborders:
+        suborder_info = order_type_models_handler[item.client_agency_name].query.filter_by(suborder_no=item.id).one()
 
 
 def _print_small_labels():
