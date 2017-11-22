34feba2092f050acf1160228e9456b6fb32ba888
Joel Castillo <jocastillo@records.nyc.gov>
2017-11-21 19:05:33 -0500

    Fixing typo

--- src/components/home.js
+++ src/components/home.js
@@ -24,25 +24,40 @@ class Home extends React.Component {
         };
 
         this.updateStatus = (suborder_number, new_status) => {
-            let status_obj = this.state.all_orders.find(obj => {return obj.suborder_number === suborder_number; });
+            let status_obj = this.state.all_orders.find(obj => {
+                return obj.suborder_number === suborder_number;
+            });
             let idx = this.state.all_orders.indexOf(status_obj);
-            let status ={"index": idx, "object": status_obj};
+            let status = {"index": idx, "object": status_obj};
             status.object.current_status = new_status;
             let _all_orders = this.state.all_orders.slice();
             _all_orders[status.index] = status.object;
             this.setState({all_orders: _all_orders});
         };
 
-        this.printLabelSheet = () => {
-        //    links to Label Sheet print
+        this.printOrderSheet = () => {
+            e.preventDefault();
+            fetch('api/v1.0/print/orders', {
+                method: "POST",
+                body: JSON.stringify({
+                    order_number: this.state.ordernumber,
+                    suborder_number: this.state.subordernumber,
+                    order_type: this.state.order_type,
+                    billing_name: this.state.billing_name,
+                    date_submitted_start: formatDate(this.dateSubmittedStart),
+                    date_submitted_end: formatDate(this.dateSubmittedEnd)
+                })
+            }).then((response) => {
+                return response.json()
+            });
         };
 
         this.printBigLabels = () => {
-        //    links to Big Label print
+            //    links to Big Label print
         };
 
         this.printSmallLabels = () => {
-        //    links to Small Label print
+            //    links to Small Label print
         };
 
         this.logOut = () => {
@@ -63,7 +78,8 @@ class Home extends React.Component {
         )).then((json) => {
             this.addOrder(json.order_count, json.suborder_count, json.all_orders);
         })
-    };
+    }
+    ;
 
 
     render() {
@@ -74,7 +90,7 @@ class Home extends React.Component {
                 suborder_number={order.suborder_number}
                 client_agency_name={order.client_agency_name}
                 billing_name={order.billing_name}
-                date_submitted={order.date_submitted.slice(0,-9)}
+                date_submitted={order.date_submitted.slice(0, -9)}
                 current_status={order.current_status}
                 updateStatus={this.updateStatus}
             />
@@ -88,18 +104,18 @@ class Home extends React.Component {
                             <Container className="sub header">Department of Records</Container>
                         </Header>
                         <Segment padded>
-                            <LoginModal this.props.authenticated />
+                            <LoginModal />
                         </Segment>
-                        <Button content='Logout' onClick={this.logOut} />
+                        <Button content='Logout' onClick={this.logOut}/>
                         <OrderForm addOrder={this.addOrder}/>
                     </Grid.Column>
                     <Grid.Column width={1}/>
                     <Grid.Column width={11}>
                         <Header as="h1" dividing textAlign="center">Order</Header>
                         <div>
-                            <Button.Group size = 'medium' floated='right'>
+                            <Button.Group size='medium' floated='right'>
                                 <Button labelPosition='left' icon='print'
-                                        content='Label Sheet' onClick={this.printLabelSheet}/>
+                                        content='Order Sheet' onClick={this.printLabelSheet}/>
                                 <Button content='Big Labels' onClick={this.printBigLabels}/>
                                 <Button content='Small Labels' onClick={this.printSmallLabels}/>
                             </Button.Group>
