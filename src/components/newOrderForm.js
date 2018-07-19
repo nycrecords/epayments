c1203b0fcf13d4236ffab584f051b8c5c46a6fd1
jchen249 <juchen@MacBook-Pro-2.local>
2018-07-19 10:45:45 -0400

    fix typo errors

--- src/components/newOrderForm.js
+++ src/components/newOrderForm.js
@@ -89,88 +89,15 @@ class NewOrderForm extends React.Component {
         this.submitFormData = (e, print) => {
             e.preventDefault();
 
-            // switch(print){
-            //     case 'orders':
-            //     case 'large_labels':
-            //     case 'small_labels':
-            //         csrfFetch('api/v1.0/print/' + print, {
-            //             method: "POST",
-            //             body: JSON.stringify({
-            //                 billingName: this.billingName,
-            //                 address: this.address,
-            //                 orderType: this.orderType,
-            //                 collection: this.collection,
-            //                 printSize: this.printSize,
-            //                 numCopies: this.numCopies,
-            //                 status: this.status
-            //                 }
-            //
-            //             )
-            //         }
-            // }
         }
     }
+    handleChange(e){
+        this.setState({ [e.target.name] : e.target.value });
 
+    }
 
     render() {
 
-
-        // const newOrder = () => (
-        //     <div>
-        //         <div>
-        //             <Grid padded columns={3}>
-        //                 <Grid.Column width={4} id="grid-column-search">
-        //
-        //                     <Link to="/">
-        //                         <Header as="h1" textAlign="center">ePayments
-        //                             <Container className="sub header">Department of Records</Container>
-        //                         </Header>
-        //                     </Link>
-        //                 </Grid.Column>
-        //             </Grid>
-        //         </div>
-        //         <div>
-        //             <Grid.Column width={1}/>
-        //             <Grid.Column width={11} id="grid-column-order">
-        //                 <Header as="h1" dividing textAlign="center">New Order</Header>
-        //
-        //
-        //                 <div>
-        //                     {/*<Container>*/}
-        //                         {/*<form className="ui form">*/}
-        //                             {/*<div className="field">*/}
-        //                                 {/*<label>First Name</label>*/}
-        //                                 {/*<input placeholder="First Name"/>*/}
-        //                             {/*</div>*/}
-        //                             {/*<div className="field">*/}
-        //                                 {/*<label>Last Name</label>*/}
-        //                                 {/*<input placeholder="Last Name"/>*/}
-        //                             {/*</div>*/}
-        //                             {/*<div className="field">*/}
-        //                                 {/*<div className="ui checkbox">*/}
-        //                                     {/*<input type="checkbox" className="hidden" readOnly="" tabIndex="0"/>*/}
-        //                                     {/*<label>I agree to the Terms and Conditions</label>*/}
-        //                                 {/*</div>*/}
-        //                             {/*</div>*/}
-        //                             {/*<button type="submit" className="ui button" role="button">Submit</button>*/}
-        //                         {/*</form>*/}
-        //                     {/*</Container>*/}
-        //
-        //
-        //
-        //                 </div>
-        //                 <div>
-        //                     <Divider clearing/>
-        //                 </div>
-        //
-        //
-        //             </Grid.Column>
-        //
-        //         </div>
-        //
-        //
-        //     </div>
-        // );
 
 
         return (
@@ -200,18 +127,18 @@ class NewOrderForm extends React.Component {
                                     <Form.Input label="Billing Name" placeholder="Billing Name" maxLength="64" width={6}
                                                 onChange={(e, {value}) => {
                                                     if (/^[a-zA-Z\s]*$/.test(value.slice(-1)) || value === '') {
-                                                        this.setState({billing_name: value})
+                                                        this.setState({billinName: value})
                                                     }
                                                 }}
-                                                value={this.state.billing_name}
+                                                value={this.state.billinName}
                                     />
                                     <Form.Input label="Address" placeholder="Address" maxLength="64" width={8}
                                                 onChange={(e, {value}) => {
                                                     if (/^[a-zA-Z\s]*$/.test(value.slice(-1)) || value === '') {
-                                                        this.setState({Address: value})
+                                                        this.setState({address: value})
                                                     }
                                                 }}
-                                                value={this.state.Address}
+                                                value={this.state.address}
                                     />
                                     <Form.Select label="Order Type" placeholder="Order Type" options={orderTypeOptions}
                                                  width={6}
@@ -238,7 +165,7 @@ class NewOrderForm extends React.Component {
                                         <Form.Radio
                                             name={"size"}
                                             label='Small'
-                                            checked={(this.state.printSize =='Small')}
+                                            checked={(this.state.printSize ==='Small')}
                                             onChange={(e, {value}) => {
                                                 value = 'Small'
                                                 this.setState({printSize: value})
@@ -248,7 +175,7 @@ class NewOrderForm extends React.Component {
                                         />
                                         <Form.Radio
                                             name={"size"}
-                                            checked={(this.state.printSize =='Medium')}
+                                            checked={(this.state.printSize ==='Medium')}
                                             label='Medium'
                                             onChange={(e, {value}) => {
                                                 value = 'Medium'
@@ -260,7 +187,7 @@ class NewOrderForm extends React.Component {
                                         <Form.Radio
                                             name={"size"}
                                             label='Large'
-                                            checked={(this.state.printSize =='Large')}
+                                            checked={(this.state.printSize ==='Large')}
                                             onChange={(e, {value}) => {
                                                 value = 'Large'
                                                 this.setState({printSize: value})
@@ -268,7 +195,7 @@ class NewOrderForm extends React.Component {
                                         />
                                     </Form.Group>
 
-                                    <Form.Select Label="Number of Copies" placeholder="Number of Copies" options={copyOptions} width={6}
+                                    <Form.Select label="Number of Copies" placeholder="Number of Copies" options={copyOptions} width={6}
                                                  onChange={(e, {value}) => {
                                                      this.setState({numCopies: value})
                                                  }}
@@ -281,7 +208,7 @@ class NewOrderForm extends React.Component {
                                                  value={this.state.status}
                                     />
                                     {/*<Message success header='Form Completed' content="You're all signed up for the newsletter" />*/}
-                                    <Button type='submit' positive floated="left" content="Place Order" onChange/>
+                                    <Button type='submit' positive floated="left" content="Place Order" onClick={this.handleSubmit}/>
                                     <Button type="reset" onClick={this.clearSelection} content="Clear"/>
                                     {/*</Form.Group>*/}
 
@@ -304,6 +231,10 @@ class NewOrderForm extends React.Component {
 
         )
     };
+
+    handleSubmit; function(){
+        console.log("Billing NAME: "+this.state.billingName);
+    }
 }
 
 
