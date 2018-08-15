d86f760db8c61f0f9049f81b51b4ed5967165153
Wallace Wong <wwong@records.nyc.gov>
2018-08-15 11:21:31 -0400

    Added tax_photo, photo_gallery, & property_card also: build files

---
+++ src/order_type/photo_gallery.js
@@ -0,0 +1,87 @@
+/**
+ * Created by walwong on 8/15/18.
+ */
+
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+class TaxPhoto extends React.Component{
+
+    render(){
+        return(
+            <div className="-order-modals">
+                {this.props.order_info['customer']['billing_name']} <br/>
+                {this.props.order_info['customer']['address']}  <br/>
+                <h2>{this.props.order_info['order_type']}</h2>
+                <strong>Order number:</strong> {this.props.order_info['customer']['order_number']}<br/>
+                <strong>Order time:</strong> {this.props.order_info['order']['date_submitted']} <br/>
+                <strong>Phone:</strong> {this.props.order_info['customer']['phone']} <br/>
+                <strong>E-mail:</strong> {this.props.order_info['customer']['email']} <br/>
+                <strong>Suborder Number</strong> {this.props.order_info['suborder_number']} <br/> <br/>
+
+                <div className="-row">
+                    <div className="-fourth"> Image Id: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info['image_id'] ? this.props.order_info['image_id'] : "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Description: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["description"] ? this.props.order_info["description"]: "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Additional Description: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["additional_description"] ? this.props.order_info["additional_description"]: "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Size: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["size"] ? this.props.order_info["size"]: "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Number of Copies: </div>
+                    <div className="-two-thirds"> {this.props.order_info["num_copies"]}</div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Mail Pickup: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["mail"] ? 'Mail' : "Pickup"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Contact Number: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info['contact_number'] ? this.props.order_info['contact_number'] : "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Personal Use Agreement: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["personal_use_agreement"] ? this.props.order_info["personal_use_agreement"]: "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Comment: </div>
+                    <div className="-two-thirds"> {this.props.order_info['comment']} </div>
+                </div>
+
+            </div>
+        )
+    };
+}
+
+
+export default TaxPhoto;
---
+++ src/order_type/property_card.js
@@ -0,0 +1,83 @@
+/**
+ * Created by walwong on 8/15/18.
+ */
+
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+class TaxPhoto extends React.Component{
+
+    render(){
+        return(
+            <div className="-order-modals">
+                {this.props.order_info['customer']['billing_name']} <br/>
+                {this.props.order_info['customer']['address']}  <br/>
+                <h2>{this.props.order_info['order_type']}</h2>
+                <strong>Order number:</strong> {this.props.order_info['customer']['order_number']}<br/>
+                <strong>Order time:</strong> {this.props.order_info['order']['date_submitted']} <br/>
+                <strong>Phone:</strong> {this.props.order_info['customer']['phone']} <br/>
+                <strong>E-mail:</strong> {this.props.order_info['customer']['email']} <br/>
+                <strong>Suborder Number</strong> {this.props.order_info['suborder_number']} <br/> <br/>
+
+                <div className="-row">
+                    <div className="-fourth"> Borough: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info['borough']  ? this.props.order_info['borough'] : "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Block: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["block"] ? this.props.order_info["block"] : "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Lot: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["lot"] ? this.props.order_info["lot"]: "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Building Number: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["building_number"] ? this.props.order_info["building_number"]: "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Street: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["street"] ? this.props.order_info["street"]: "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Description: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["description"] ? this.props.order_info["description"]: "n/a"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Certified: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["mail"] ? 'Certified' : "Not Certified"}
+                    </div>
+                </div>
+
+                <div className="-row">
+                    <div className="-fourth"> Mail Pickup: </div>
+                    <div className="-two-thirds">
+                        {this.props.order_info["mail"] ? 'Mail' : "Pickup"}
+                    </div>
+                </div>
+            </div>
+        )
+    };
+}
+
+
+export default TaxPhoto;
