f48c3a0f3943f3b5bcd662c41863685afd8a5671
Gary Zhou <gzhou@records.nyc.gov>
2022-08-23 16:40:37 -0400

    Update Pipfile & Pipfile.lock

--- app/main/views.py
+++ app/main/views.py
@@ -34,6 +34,6 @@ def import_tar():
 
 
 # noinspection PyTypeChecker,PyTypeChecker
-@main.route('/static/files/<string:filename>', methods=['GET', 'POST'])
-def download(filename):
-    return send_from_directory(current_app.config["PRINT_FILE_PATH"], filename, as_attachment=True)
+# @main.route('/static/files/<string:filename>', methods=['GET', 'POST'])
+# def download(filename):
+#     return send_from_directory(current_app.config["PRINT_FILE_PATH"], filename, as_attachment=True)
